a0eda8d8422cd9ada25c9a7735d2b6b026b95601
zvenrm <zvennnnnn@gmail.com>
2022-05-05 12:18:11 +0300

    feat: create keydown for caps, shift and lang change

--- keyboard/index.js
+++ keyboard/index.js
@@ -3,68 +3,114 @@ import createInput from './js/textInput.js';
 import createKeyboard from './js/keyboard.js';
 import keyData from './js/keydata.js';
 
-
 createContainerAndHeader();
 createInput();
 createKeyboard();
 
-const rows = document.querySelectorAll('.row')
-const input = document.querySelector('.input')
-const caps = document.querySelector('.CapsLock')
+const rows = document.querySelectorAll('.row');
+const input = document.querySelector('.input');
+const caps = document.querySelector('.CapsLock');
 
+let lang = 'en';
 
-rows.forEach(e => {
-  e.addEventListener('click', event => {
-    if(event.target.textContent.length === 1){
-      input.value += event.target.textContent
+function shiftChange(e) {
+  if (e.type === 'keydown') {
+    if (!caps.classList.contains('key_caps')) {
+      for (let i = 0; i < 5; i += 1) {
+        for (let j = 0; j < rows[i].children.length; j += 1) {
+          if (Object.keys(keyData[i][j]).includes('shift')) {
+            rows[i].children[j].innerHTML = keyData[i][j].shift[lang];
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < 5; i += 1) {
+        for (let j = 0; j < rows[i].children.length; j += 1) {
+          if (Object.keys(keyData[i][j]).includes('shift') && rows[i].children[j].textContent.length === 1) {
+            rows[i].children[j].innerHTML = keyData[i][j].shift[lang].toLowerCase();
+          }
+        }
+      }
     }
-  })
-})
+  }
+  if (e.type === 'keyup') {
+    if (!caps.classList.contains('key_caps')) {
+      for (let i = 0; i < 5; i += 1) {
+        for (let j = 0; j < rows[i].children.length; j += 1) {
+          rows[i].children[j].innerHTML = keyData[i][j].key[lang];
+        }
+      }
+    } else {
+      for (let i = 0; i < 5; i += 1) {
+        for (let j = 0; j < rows[i].children.length; j += 1) {
+          if (rows[i].children[j].textContent.length === 1) {
+            rows[i].children[j].innerHTML = keyData[i][j].key[lang].toUpperCase();
+          }
+        }
+      }
+    }
+  }
+}
 
+function capsChange() {
+  if (!caps.classList.contains('key_caps')) {
+    caps.classList.add('key_caps');
+    for (let i = 0; i < 5; i += 1) {
+      for (let j = 0; j < rows[i].children.length; j += 1) {
+        if (rows[i].children[j].textContent.length === 1) {
+          rows[i].children[j].innerHTML = rows[i].children[j].innerHTML.toUpperCase();
+        }
+      }
+    }
+  } else {
+    caps.classList.remove('key_caps');
+    for (let i = 0; i < 5; i += 1) {
+      for (let j = 0; j < rows[i].children.length; j += 1) {
+        if (rows[i].children[j].textContent.length === 1) {
+          rows[i].children[j].innerHTML = rows[i].children[j].innerHTML.toLowerCase();
+        }
+      }
+    }
+  }
+}
 
+rows.forEach((e) => {
+  e.addEventListener('click', (event) => {
+    if (event.target.textContent.length === 1) {
+      input.value += event.target.textContent;
+    }
+  });
+});
 
 document.addEventListener('keydown', (e) => {
   if (e.altKey && e.ctrlKey) {
-    console.log(123)
-    
-    for(let i = 0; i < 5; i++){
-      for(let j = 0; j < rows[i].children.length; j++){
-        rows[i].children[j].innerHTML = keyData[i][j].key.ru
-      }
-    }
-  }
-  document.querySelector(`.${e.code}`).classList.add('key-active')
-  if(e.key === 'Shift'){
-    console.log(keyData[0][1].shift.en)
-    for(let i = 0; i < 5; i++){
-      for(let j = 0; j < rows[i].children.length; j++){
-        if(keyData[i][j].hasOwnProperty('shift')){
-          rows[i].children[j].innerHTML = keyData[i][j].shift.en
+    if (lang === 'en') {
+      for (let i = 0; i < 5; i += 1) {
+        for (let j = 0; j < rows[i].children.length; j += 1) {
+          rows[i].children[j].innerHTML = keyData[i][j].key.ru;
         }
       }
-    }
-  }
-  else if(e.key === 'CapsLock'){
-    caps.classList.add('key_caps')
-    for(let i = 0; i < 5; i++){
-      for(let j = 0; j < rows[i].children.length; j++){
-        if(keyData[i][j].hasOwnProperty('shift')){
-          rows[i].children[j].innerHTML = keyData[i][j].shift.en
+      lang = 'ru';
+    } else {
+      for (let i = 0; i < 5; i += 1) {
+        for (let j = 0; j < rows[i].children.length; j += 1) {
+          rows[i].children[j].innerHTML = keyData[i][j].key.en;
         }
       }
+      lang = 'en';
     }
   }
-  
-})
+  document.querySelector(`.${e.code}`).classList.add('key-active');
+  if (e.key === 'Shift') {
+    shiftChange(e);
+  } else if (e.key === 'CapsLock') {
+    capsChange();
+  }
+});
 
 document.addEventListener('keyup', (e) => {
-  document.querySelector(`.${e.code}`).classList.remove('key-active')
-  if(e.key === 'Shift'){
-    for(let i = 0; i < 5; i++){
-      for(let j = 0; j < rows[i].children.length; j++){
-        rows[i].children[j].innerHTML = keyData[i][j].key.en
-      }
-    }
+  document.querySelector(`.${e.code}`).classList.remove('key-active');
+  if (e.key === 'Shift') {
+    shiftChange(e);
   }
-})
-
+});
