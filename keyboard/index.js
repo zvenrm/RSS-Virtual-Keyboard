b1fbb8d2b15cd1677b3fdb8b696dbc570de1050d
zvenrm <zvennnnnn@gmail.com>
2022-05-09 23:11:59 +0300

    fix: fix shift and arrow keys

--- keyboard/index.js
+++ keyboard/index.js
@@ -67,7 +67,6 @@ if (localStorage.getItem('checkCaps') === 'true') {
 } else {
   checkCaps = false;
 }
-let shft = false;
 input.focus();
 let cursor = input.selectionStart;
 
@@ -244,8 +243,19 @@ function langChange() {
     lang = 'en';
     document.querySelector('.key_lang').textContent = 'en';
   }
-  shft = false;
 }
+
+document.addEventListener('mousedown', (e) => {
+  if (e.target.textContent === 'Shift') {
+    shiftDown();
+  }
+});
+
+document.addEventListener('mouseup', (e) => {
+  if (e.target.textContent === 'Shift') {
+    shiftUp();
+  }
+});
 
 rows.forEach((e) => {
   e.addEventListener('click', (event) => {
@@ -271,10 +281,6 @@ rows.forEach((e) => {
         cursor += 1;
         input.setSelectionRange(cursor, cursor);
       }
-      if (shft === true) {
-        shiftUp();
-        shft = false;
-      }
     } else if (event.target.textContent === 'CapsLk') {
       capsChange();
     } else if (event.target.textContent === 'Tab') {
@@ -287,14 +293,6 @@ rows.forEach((e) => {
       enterChange();
     } else if (event.target.textContent === 'en' || event.target.textContent === '🇷🇺') {
       langChange();
-    } else if (event.target.textContent === 'Shift') {
-      if (shft === false) {
-        shiftDown();
-        shft = true;
-      } else {
-        shiftUp();
-        shft = false;
-      }
     }
   });
 });
@@ -325,7 +323,7 @@ document.addEventListener('keydown', (e) => {
   } else if (e.key === 'Enter') {
     e.preventDefault();
     enterChange();
-  } else if (e.key.length === 1) {
+  } else if (e.key.length === 1 || e.key.includes('Arrow')) {
     e.preventDefault();
     input.focus();
     if (input.selectionStart !== input.selectionEnd) {
@@ -345,10 +343,6 @@ document.addEventListener('keydown', (e) => {
       input.value = text;
       cursor += 1;
       input.setSelectionRange(cursor, cursor);
-    }
-    if (shft === true) {
-      shiftUp();
-      shft = false;
     }
   }
 });
